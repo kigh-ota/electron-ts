f7234b949c992f51b8eb61288cab052488d356d8
Kaiichiro Ota <kigh.ota@gmail.com>
2020-04-23 00:46:57 +0900

    suppress eslint warnings

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = common.map(cfg =>
+module.exports = common.map((cfg) =>
   merge(cfg, {
     mode: 'development',
     devtool: 'inline-source-map',
